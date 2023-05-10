beb548fd4fd727a053b2360b38f4ed40b11de71f
Oliwia Milewska <99407556+OliwiaMilewska@users.noreply.github.com>
2023-05-10 16:31:11 +0200

    Changes in description.

--- Scripts/deliveredOldOrders.js
+++ Scripts/deliveredOldOrders.js
@@ -1,4 +1,8 @@
-//This script sets a variable oneMonthAgo to be the current date minus one month. It then finds all documents in the 'orders' collection that have a status of 'delivered' and an 'orderDate' that is older than oneMonthAgo. For each of these documents, it creates a new document in the 'history' collection with the same 'orderId', 'customerId', 'products', and a 'total' field calculated by summing the prices of each product ordered multiplied by its amount. The 'realizationDate' field is set to the current date. Finally, it deletes the original document from the 'orders' collection.
+/*This script sets a variable oneMonthAgo to be the current date minus one month.
+It then finds all documents in the 'orders' collection with status of 'delivered' and an 'orderDate' that is older than oneMonthAgo. 
+For each of these documents, it creates a new document in the 'history' collection with the same 'orderId', 'customerId', 'products', and a 'total' field 
+calculated by summing the prices of each product ordered multiplied by its amount. The 'realizationDate' field is set to the current date.
+Finally, it deletes the original document from the 'orders' collection.*/
 
 var db = connect("127.0.0.1:27017/projectcrm");
 
